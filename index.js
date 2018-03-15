7ddbb2106bc72c26bda448800b6c8489d9ca7ccc
stefan <stmu@gmx.net>
2018-03-15 20:26:49 +0100

    ..

--- index.js
+++ index.js
@@ -1,32 +1,56 @@
 "use strict";
 var APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
 var SKILL_STATES = {
-    SPORT: "_STARTMODE",
+    SLOGAN: "_STARTMODE",
     HELP: "_HELPMODE" // The user is asking for help.
 };
 
-var exercises = [
-    "kniebeuge",
-    "liegestütze",
-    "Sit Ups",
-    "rumpfbeugen",
-]
+var slogans = [
+    "Glaube an Wunder, Liebe und Glück! Schau nach vorn und nicht zurück! Tu was du willst, und steh dazu; denn dein Leben lebst nur du!",
+    "Das Glück ist das einzige, was sich verdoppelt, wenn man es teilt.",
+    "Wir leben zu sehr in der Vergangenheit, haben Angst vor der Zukunft und vergessen dabei völlig, die Gegenwart zu genießen.",
+    "Denke nicht so oft an das, was dir fehlt,sondern an das, was du hast.",
+    "Das Vertrauen ist eine zarte Pflanze. Ist es zerstört, so kommt es so bald nicht wieder.",
+    "Das sind die Starken der Welt: die unter Tränen lachen, eigene Sorgen verbergen, und andere glücklich machen.",
+    "Wer dein Schweigen nicht versteht, versteht deine Worte auch nicht.",
+    "Die, die nichts zu sagen haben, reden viel. Die, die was zu sagen haben, hingegen kaum.",
+    "Tue nie etwas halb, sonst verlierst du mehr, als du je wieder einholen kannst.",
+    "Wer einen Fluss überquert, muss die eine Seite verlassen.",
+    "Was immer du tun kannst oder erträumst zu können, beginne es jetzt. ",
+    "Die Definition von Wahnsinn ist, immer das Gleiche zu tun und andere Ergebnisse zu erwarten.",
+    "Zwei Dinge sind unendlich: Das Universum und die menschliche Dummheit. Aber beim Universum bin ich mir nicht ganz sicher.",
+    "Wenn zwei Menschen immer die gleiche Meinung haben, taugen beide nichts.",
+    "Ein Zyniker ist ein Mensch, der die Dinge so sieht, wie sie sind, und nicht, wie sie sein sollten",
+    "Die Wahrheit liegt meist am Rande, nicht in der Mitte.",
+    "Wer die Wahrheit hören will, den sollte man vorher fragen, ob er sie ertragen kann. ",
+    "Wir denken selten an das, was wir haben, aber immer an das, was uns fehlt.",
+    "Die Liebe stirbt niemals an Hunger, wohl aber an Übersättigung.",
+    "Alt ist man erst dann, wenn man an der Vergangenheit mehr Freude hat als an der Zukunft.",
+    "Das sind die Starken der Welt: die unter Tränen lachen, eigene Sorgen verbergen, und andere glücklich machen.",
+    "Was Du hast, können viele haben. Doch was Du bist, kann keiner sein.",
+    "Was Du hast, können viele haben. Doch was Du bist, kann keiner sein.",
+    "Kein Mensch war ohne Grund in deinem Leben. Der eine war ein Geschenk, der andere eine Lektion.",
+    "Bei der falschen Person kannst du nichts richtig machen. Bei der richtigen Person kannst du nichts falsch machen.",
+    "Am Ende zählen nur die Menschen, denen man auf die Frage 'Alles okay bei dir?' mit der Wahrheit antwortet...",
+    "Lieber Fehler machen, als so zu tun, als sei man perfekt!",
+    "Heute kennt man von allem den Preis, von nichts den Wert.",
+    "Der Schwache kann nicht verzeihen. Verzeihen ist eine Eigenschaft des Starken.",
+    "Das Geheimnis des Glücks liegt nicht im Besitz, sondern im Geben. Wer andere glücklich macht, wird glücklich.",
+    "Es gibt viele Wege zum Glück. Einer davon ist aufhören zu jammern.",
+    "Je weniger Bedürfnisse ihr habt, desto freier seid ihr.",
+    "Das Glück gehört denen, die sich selbst genügen. Denn alle äußeren Quellen des Glückes und Genusses sind ihrer Natur nach höchst unsicher, misslich, vergänglich und dem Zufall unterworfen.",
+    "Die glücklichsten Menschen der Welt haben keine geteerten Straßen.",
+    "Die glücklichsten Menschen der Welt haben keine geteerten Straßen.",
+    "Was ist Glück?, Glück ist: Sich kratzen können, wo es einen juckt.",
+    "Turne bis zur Urne.",
+    "Ein langes Glück verliert schon allein durch seine Dauer.",
+    "Man lebt ruhiger, wenn man nicht alles sagt, was man weiß, nicht alles glaubt, was man hört und über den Rest einfach nur lächelt.",
+    "Wenn du wissen willst, wer dich beherrscht, mußt du nur herausfinden, wen du nicht kritisieren darfst.",
+    "Die fast unlösbare Aufgabe besteht darin, sich weder von der Macht der anderen, noch von der eigenen Ohnmacht dumm machen zu lassen.",
+    "Der Versuch den Himmel auf Erden zu verwirklichen, produzierte stets die Hölle.",
+    "Bildung ist das, was übrigbleibt, wenn man alles vergessen hat, was man in der Schule lernte.",
+    "",
 
-var greetings = [
-    "Hallo ich helfe dir fit zu bleiben. Dazu mach %s %s.",
-    "Ich helfe dir fit zu werden. Dazu führe %s %s aus.",
-    "Führe %s %s aus. Das hält dich fit",
-    "Hallo, Kreise 20 sekunden mit dem linken arm und dann ",
-    "Hallo, Kreise 20 sekunden mit dem rechten arm und dann ",
-    "Hallo, Kreise 20 sekunden mit dem becken und dann ",
-    "Hallo, Kreise 20 sekunden mit dem rechten fuß und dann ",
-    "Hallo, Kreise 20 sekunden mit dem linken fuß und dann ",
-    "Hallo, Stehe 20 sekunden auf dem linken bein und dann ",
-    "Hallo, Stehe 20 sekunden auf dem rechten bein und dann ",
-    "Hallo, Kreise 20 sekunden mit dem Kopf und dann ",
-    "Hallo, Nicke 20 sekunden mit dem Kopf und dann ",
-    "Hallo, Kreise 20 sekunden mit dem linken bein und dann ",
-    "Hallo, Kreise für 20 sekunden eine acht mit deinem becken und dann ",
 ]
 
 var Alexa = require("alexa-sdk");
@@ -41,29 +65,27 @@ exports.handler = function (event, context, callback) {
 };
 
 var sportHandler = Alexa.CreateStateHandler(SKILL_STATES.SPORT, {
-    "exercise": function () {
+    "slogan": function () {
       
-        var greetingsNumber = Math.floor((Math.random() * greetings.length));
-        var exerciseNumber = Math.floor((Math.random() * exercises.length));
-        var repetition = Math.floor((Math.random() * 10) + 1);
-       
-        var speechout = util.format(greetings[greetingsNumber], repetition, exercises[exerciseNumber]); 
-        console.log(speechout);
+        var sloganNumber = Math.floor((Math.random() * slogan.length));
+
+        var speechout = util.format(slogans[greetingsNumber]); 
+    
         this.emit(':tell', speechout);
     }   
 });
 
 const newSessionHandlers = {
     'LaunchRequest': function () {
-        this.handler.state = SKILL_STATES.SPORT;
-        this.emitWithState("exercise");
+        this.handler.state = SKILL_STATES.SLOGAN;
+        this.emitWithState("slogan");
     },
     "AMAZON.HelpIntent": function () {
         this.emit(":tell", "");
     },
     "Unhandled": function () {
-        this.handler.state = SKILL_STATES.SPORT;
-        this.emitWithState("exercise");
+        this.handler.state = SKILL_STATES.SLOGAN;
+        this.emitWithState("slogan");
     },
 
 };
